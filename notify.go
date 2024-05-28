78c26f6fa2b925be5e6fd35014dd900391752a0d
ttoonn112 <ttoonn112@gmail.com>
2024-05-28 17:23:57 +0700

    Update notify.go

--- notify.go
+++ notify.go
@@ -1,12 +1,14 @@
 package ktgolib
 
 import (
+  //"bytes"
+	"context"
   "github.com/utahta/go-linenotify"
 )
 
-func Notify_ToLine(line_token string, msg string) (string, string) {
-	linenotify := linenotify.New()
-	response, err := linenotify.Notify(line_token, msg, "", "", nil)
+func Notify_ToLine(token string, msg string) (string, string) {
+	c := linenotify.NewClient()
+	response, err := c.Notify(context.Background(), token, msg, "", "", nil)
 	if err != nil {
 		return "", err.Error()
 	}else if response.Status != 200 {
@@ -14,4 +16,8 @@ func Notify_ToLine(line_token string, msg string) (string, string) {
 	}else{
 		return I_S(response.RateLimit.Remaining)+"/"+I_S(response.RateLimit.Limit), ""
 	}
+
+	//c.Notify(context.Background(), token, "hello world", "", "", nil)
+	//c.Notify(context.Background(), token, "hello world", "http://localhost/thumb.jpg", "http://localhost/full.jpg", nil)
+	//c.Notify(context.Background(), token, "hello world", "", "", bytes.NewReader([]byte("image bytes")))
 }
