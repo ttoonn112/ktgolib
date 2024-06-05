87667685fc12a70d32708eaafc51b8e0838f108b
ttoonn112 <ttoonn112@gmail.com>
2024-06-05 15:30:04 +0700

    Update sql.go

--- sql.go
+++ sql.go
@@ -15,6 +15,14 @@ func AddSqlFilter(field_name string, value string) string{
 	return ""
 }
 
+func AddSqlLikeFilter(field_name string, value string) string{
+	if value != "All" && value != "" {
+		filter := " and "+field_name+" like '%"+value+"%' "
+		return filter
+	}
+	return ""
+}
+
 func AddSqlDateRangeFilter(field_name string, start_date string, end_date string) string{
 	if start_date != "" && FirstXChar(start_date,10) != "0000-00-00" && end_date != "" && FirstXChar(end_date,10) != "0000-00-00" {
 		filter := " and "+field_name+" between '"+start_date+"' and '"+end_date+"' "
