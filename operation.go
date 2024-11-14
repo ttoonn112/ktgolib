63c713ec12056a00850bd8e877d2ad5960804b08
ttoonn112 <ttoonn112@gmail.com>
2024-11-14 10:23:34 +0700

    Update operation.go

--- operation.go
+++ operation.go
@@ -53,3 +53,27 @@ func Operation_IsLimitExceededBySec(username string, operation string, key strin
 
   return result
 }
+
+// sec = "จำนวนวินาทีที่ต้องรอ ก่อนทำ Operation ครั้งถัดไป"
+// token = จำนวนครั้งที่ buffer ไว้ทุกๆ sec
+func Operation_IsLimitExceededBySecToken(username string, operation string, key string, sec int, token int) bool {
+  mu.Lock()
+  defer mu.Unlock()
+
+  if userOperationLimiters[username] == nil {
+    userOperationLimiters[username] = make(map[string]*rate.Limiter)
+  }
+
+  limiter, exists := userOperationLimiters[username][operation]
+  if !exists {
+    limiter = rate.NewLimiter(rate.Every(time.Duration(sec) * time.Second), token)       // จะสามารถทำงานได้ เพียง 1 ครั้งทุก ๆ [sec] วินาที โดยไม่มีการสะสม Token เพื่อทำงานหลายครั้งต่อเนื่อง
+    userOperationLimiters[username][operation] = limiter
+  }
+
+  result := !limiter.Allow()
+  if result {
+    LogHidden(operation, username, key, "", "Operation_IsLimitExceededBySecToken")
+  }
+
+  return result
+}
