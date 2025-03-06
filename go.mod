456a94ec13e6dea7b5df78f7bddf139ea5f01053
ttoonn112 <ttoonn112@gmail.com>
2025-03-06 15:41:39 +0700

    add telegram

--- go.mod
+++ go.mod
@@ -1,6 +1,8 @@
 module github.com/ttoonn112/ktgolib
 
-go 1.19
+go 1.23.0
+
+toolchain go1.23.4
 
 require (
 	github.com/kr/pretty v0.3.1
@@ -8,7 +10,14 @@ require (
 )
 
 require (
+	github.com/dustin/go-humanize v1.0.1 // indirect
+	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible // indirect
+	github.com/google/uuid v0.0.0-20171113160352-8c31c18f31ed // indirect
 	github.com/jasonlvhit/gocron v0.0.1 // indirect
 	github.com/kr/text v0.2.0 // indirect
+	github.com/pkg/errors v0.8.1-0.20161029093637-248dadf4e906 // indirect
 	github.com/rogpeppe/go-internal v1.9.0 // indirect
+	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
+	github.com/utahta/go-linenotify v0.5.0 // indirect
+	golang.org/x/time v0.11.0 // indirect
 )
--- notify.go
+++ notify.go
@@ -4,6 +4,7 @@ import (
   //"bytes"
 	"context"
   "github.com/utahta/go-linenotify"
+  "github.com/go-telegram-bot-api/telegram-bot-api"
 )
 
 func Notify_ToLine(token string, msg string) (string, string) {
@@ -20,4 +21,23 @@ func Notify_ToLine(token string, msg string) (string, string) {
 	//c.Notify(context.Background(), token, "hello world", "", "", nil)
 	//c.Notify(context.Background(), token, "hello world", "http://localhost/thumb.jpg", "http://localhost/full.jpg", nil)
 	//c.Notify(context.Background(), token, "hello world", "", "", bytes.NewReader([]byte("image bytes")))
+}
+
+func Notify_ToTelegram(botToken string, chatID int64, message string) error {
+	bot, err := tgbotapi.NewBotAPI(botToken)
+	if err != nil {
+		return err
+	}
+	msg := tgbotapi.NewMessage(chatID, message)
+	_, err = bot.Send(msg)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Notify_TelegramGetUpdates(botToken string) map[string]interface{} {
+	url := "https://api.telegram.org/bot"+botToken+"/getUpdates"
+	result := Http_Get(url)
+	return result
 }
